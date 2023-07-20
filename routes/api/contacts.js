d981734f41e22203d286ebdbfd926a9bfb67f338
mariastetciuk <114301391+mariastetciuk@users.noreply.github.com>
2023-07-20 18:11:41 +0300

    add decorators

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,97 +1,27 @@
-import express, { response } from 'express';
-import contactsService from '../../models/contacts.js';
-import { HttpError } from '../../helpers/index.js';
-import Joi from 'joi';
+import express from 'express';
+import contactsControllers from '../../controllers/contacts-controller.js';
+import { validateBody } from '../../decorators/index.js';
+import { emptyBody, contactsAdd } from '../../schemas/contacts-schemas.js';
 
 const router = express.Router();
 
-const contactsAddSchema = Joi.object({
-  name: Joi.string()
-    .min(3)
-    .required()
-    .messages({ 'any.required': 'missing required name field' }),
-  email: Joi.string()
-    .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } })
-    .required()
-    .messages({ 'any.required': 'missing required email field' }),
-  phone: Joi.number()
-    .min(9)
-    .required()
-    .messages({ 'any.required': 'missing required phone field' }),
-});
+router.get('/', contactsControllers.getAll);
 
-router.get('/', async (req, res, next) => {
-  try {
-    const result = await contactsService.listContacts();
-    res.json(result);
-  } catch (error) {
-    next(error);
-  }
-});
+router.get('/:contactId', contactsControllers.getById);
 
-router.get('/:contactId', async (req, res, next) => {
-  try {
-    const { contactId } = req.params;
-    const result = await contactsService.getContactById(contactId);
+router.post(
+  '/',
+  validateBody(emptyBody),
+  validateBody(contactsAdd),
+  contactsControllers.add
+);
 
-    if (!result) {
-      throw HttpError(404);
-    }
+router.delete('/:contactId', contactsControllers.deleteById);
 
-    res.json(result);
-  } catch (error) {
-    next(error);
-  }
-});
-
-router.post('/', async (req, res, next) => {
-  try {
-    const { error } = contactsAddSchema.validate(req.body);
-    if (error) {
-      throw HttpError(400, error.message);
-    }
-    const result = await contactsService.addContact(req.body);
-    res.status(201).json(result);
-  } catch (error) {
-    next(error);
-  }
-});
-
-router.delete('/:contactId', async (req, res, next) => {
-  try {
-    const { contactId } = req.params;
-    const result = await contactsService.removeContact(contactId);
-
-    if (!result) {
-      throw HttpError(404);
-    }
-
-    res.json({ message: 'contact deleted' });
-  } catch (error) {
-    next(error);
-  }
-});
-
-router.put('/:contactId', async (req, res, next) => {
-  try {
-    const { name, email, phone } = req.body;
-    if (!name && !email && !phone) {
-      throw HttpError(400, 'missing fields');
-    }
-
-    const { error } = contactsAddSchema.validate(req.body);
-    if (error) {
-      throw HttpError(400, error.message);
-    }
-    const { contactId } = req.params;
-    const result = await contactsService.updateContact(contactId, req.body);
-    if (!result) {
-      throw HttpError(404);
-    }
-    res.json(result);
-  } catch (error) {
-    next(error);
-  }
-});
+router.put(
+  '/:contactId',
+  validateBody(contactsAdd),
+  contactsControllers.updateById
+);
 
 export default router;
